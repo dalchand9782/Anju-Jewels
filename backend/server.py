1d6c4bd771f9c8fabafc6db2c07c33afb3d853d7
Dalchand saini <92579208+dalchand9782@users.noreply.github.com>
2026-01-29 22:42:01 +0530

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -527,8 +527,8 @@ app.include_router(api_router)
 
 app.add_middleware(
     CORSMiddleware,
+    allow_origins=["*"],  # Testing ke liye "*" sabse best hai, isse error khatam ho jayega
     allow_credentials=True,
-    allow_origins=os.environ.get('CORS_ORIGINS', '*').split(','),
     allow_methods=["*"],
     allow_headers=["*"],
 )
